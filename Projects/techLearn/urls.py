6b31ffd0913077b7ad4fc6b43a6a11c377fb51a4
ywatch <94308024+ywatch@users.noreply.github.com>
2023-05-01 12:49:48 +0100

    home valide

--- Projects/techLearn/urls.py
+++ Projects/techLearn/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from techLearn.views import home
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('compte.urls')),
-    
+    path('', home),
+
 ]
