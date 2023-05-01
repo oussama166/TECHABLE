31edb4c668a6adb6b36bb7e092e274ee1fd213d2
ywatch <94308024+ywatch@users.noreply.github.com>
2023-05-01 12:36:11 +0100

    inscriptin model + inscription final

--- Projects/compte/views.py
+++ Projects/compte/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import get_user_model,login
 # Create your views here.
+USER= get_user_model()
 def connection(request):
   return render(request,'connection.html',{})
 
 def inscription(request):
+  if request.method == "POST":
+    username= request.POST.get("name")
+    password= request.POST.get("password")
+    email= request.POST.get("email")
+    user=USER.objects.create_user(
+      username=username,
+      password=password,
+      email=email
+    )
+    login(request,user)
+    return render(request,'connection.html',{})
+
   return render(request,'inscription.html',{})
