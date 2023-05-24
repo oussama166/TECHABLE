0684fd2261b65930d43585a798a63e546f3f3907
ywatch <94308024+ywatch@users.noreply.github.com>
2023-05-24 18:14:50 +0100

    100%complet admin+inscription+connexion

--- Projects/compte/views.py
+++ Projects/compte/views.py
@@ -4,16 +4,18 @@ from django.shortcuts import render,redirect
 from techLearn.views import home
 from django.contrib.auth import get_user_model,login,authenticate,logout
 from compte.models import utilisateur
+from django.http import HttpResponse
 # Create your views here.
+User=get_user_model()
 def connection(request):
     if request.method == "POST":
         username= request.POST.get("name")
         email= request.POST.get("email")
         password= request.POST.get("password")
-        if utilisateur.objects.filter(nameU=username,emailU=email,passwordU=password).exists() :
-          us=username
-          em=email
-          return render(request,'admin.html',{"username":us,"email":em})
+        user1= authenticate(username=username,email=email,password=password)
+        if user1 :
+          login(request,user1)
+          return redirect('admin')
     return render(request,'connection.html',{})
 
 def inscription(request):
@@ -21,9 +23,10 @@ def inscription(request):
     username= request.POST.get("name")
     password= request.POST.get("password")
     email= request.POST.get("email")
-    u=utilisateur(nameU=username,emailU=email,passwordU=password)
-    u.save()
-    return render(request,'connection.html',{})
+    user=User.objects.create_user(username=username,email=email,password=password)
+    # u=utilisateur(nameU=username,emailU=email,passwordU=password)
+    # u.save()
+    return redirect('connection')
   return render(request,'inscription.html',{})
 
 def admin(request):
@@ -32,10 +35,12 @@ def admin(request):
         email= request.POST.get("UserMail")
         password= request.POST.get("currPass")
         passwordc= request.POST.get("changePass")
-        if utilisateur.objects.filter(nameU=username,emailU=email,passwordU=password).exists() :
-          u=utilisateur.objects.all().get(nameU='abdo mendoubi')
-          print(u)
-          # u.passwordU='222'
-          # u.save()          
-          return render(request,'connection.html',{})
-    return render(request,'admin.html',{})
+        user1=authenticate(username=username,email=email,password=password)
+        if user1 :
+          u=User.objects.all().get(username=username)
+          u.set_password(passwordc)
+          u.save()   
+          return redirect('connection')
+          # return render(request,'connection.html',{})
+        # return HttpResponse('Success')
+    return render(request,'admin.html',{})          
